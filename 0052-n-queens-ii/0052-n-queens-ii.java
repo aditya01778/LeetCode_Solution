faf05ff2398c6e1843669b55be98977ddfb8207f
aditya <adityakumar2007mn@gmail.com>
2026-09-01 23:47:39 +0530

    Time: 2 ms (57.96%), Space: 42.3 MB (25.39%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,57 @@
+class Solution {
+    private int count = 0;
+
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        nQueen(board, 0);
+        return count;
+    }
+
+    public boolean isSafe(char[][] board, int row, int col) {
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void nQueen(char[][] board, int row) {
+        // base case
+        if (row == board.length) {
+            count++;
+            return;
+        }
+
+        // recursive call
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueen(board, row + 1); // recursion call
+                board[row][j] = '.'; // backtracking step
+            }
+        }
+    }
+}
