96b5a42ebe90dbcd52892be9938263b98666c393
aditya <adityakumar2007mn@gmail.com>
2026-09-25 13:05:10 +0530

    Time: 0 ms (100%), Space: 43.6 MB (24.22%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        // loop to traverse element from last 
+        for(int i=digits.length-1; i>=0; i--) {
 
-        // Start from the last digit
-        for (int i = digits.length - 1; i >= 0; i--) {
-
-            // If digit is less than 9, just add 1
-            if (digits[i] < 9) {
+            // case1
+            if(digits[i]<9) {
                 digits[i]++;
                 return digits;
             }
+            // cse2
+            else{
+                digits[i] = 0;
+            }
 
-            // If digit is 9, make it 0 and carry goes left
-            digits[i] = 0;
         }
-
-        // If all digits were 9
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
-
-        return result;
+        // case3 sara element 9 hai 
+        int newNumber[] = new int[digits.length+1];
+        newNumber[0] = 1;
+        return newNumber;
+        
     }
 }
