40e827c07ca64b6bc5623d36da2e779e4d70570e
aditya <adityakumar2007mn@gmail.com>
2026-09-27 01:07:28 +0530

    Time: 5 ms (84.06%), Space: 45.5 MB (97.93%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isPalindrome(int x) {
-
-        // Negative numbers are not palindrome
-        if (x < 0) {
+        // agar number negative hai to false
+    if(x < 0) {
             return false;
         }
+    int original = x;
+    int reverse = 0;
+        // agar positive hai t
+        while(x != 0) {
 
-        int original = x;
-        int reverse = 0;
-
-        while (x != 0) {
-            int digit = x % 10;          // get last digit
+            // last digit nikalna
+            int digit = x % 10;
+            // last ke digit ko store kre hai reverse ke 1st place pe
             reverse = reverse * 10 + digit;
-            x = x / 10;                  // remove last digit
+            // remove krenge lastelement ko x se
+            x = x / 10;
         }
-
         return original == reverse;
     }
 }
